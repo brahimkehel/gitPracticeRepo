ba6bf1740642bdcecb236eb7e6ec98dcb723f9b5
kehel brahim <brahimkehel@gmail.com>
2022-03-14 14:44:34 +0100

    adding bird greeting mehtod

--- Animal.java
+++ Animal.java
@@ -1,10 +1,21 @@
 class Animal{
 
-    public static defaultGreeting(){
+    public static String defaultGreeting(){
         return "Hello!";
     }
 
+    public static String birdGreeting(){
+        return "Tweet!";
+    }
+
     public static void main(String[] args){
-        System.out.println(defaultGreeting());
+        String firstArgument=args[0];
+        String output="";
+        if(firstArgument.equalsIgnoreCase("bird"))
+            output=birdGreeting();
+        else
+            output=defaultGreeting();
+
+        System.out.println(output);
     }
 }
