5c61a521be3d2624668107f1a1d7b9242ac8af57
kehel brahim <brahimkehel@gmail.com>
2022-03-14 14:46:40 +0100

    adding cat greeting method

--- Animal.java
+++ Animal.java
@@ -1,10 +1,21 @@
 class Animal{
 
-    public static defaultGreeting(){
+    public static String defaultGreeting(){
         return "Hello!";
     }
 
+    public static String catGreeting(){
+        return "Meow!";
+    }
+
     public static void main(String[] args){
-        System.out.println(defaultGreeting());
+        String firstArgument=args[0];
+        String output="";
+        if(firstArgument.equalsIgnoreCase("cat"))
+            output=catGreeting();
+        else
+            output=defaultGreeting();
+
+        System.out.println(output);
     }
 }
